1c54fbebd7022dba63d69ddf91374d1792ad2cfa
Tony Khalilollahi <tkhalilo@ucsc.edu>
2020-08-01 00:27:18 -0700

    game update

--- blog/models.py
+++ blog/models.py
@@ -46,6 +46,12 @@ class Game(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    difficulty = models.PositiveIntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    difficultyLeft = models.PositiveIntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    trailer1 = models.CharField(default="none", max_length=200)
+    trailer2 = models.CharField(default="none", max_length=200)
+    trailer3 = models.CharField(default="none", max_length=200)
+    link = models.CharField(max_length=200)
 
     class Meta:
         ordering = ['-created_on']
