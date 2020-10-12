fdeb83f30680a8e37c7807414cead25ee2417049
Tony Khalilollahi <tkhalilo@ucsc.edu>
2020-10-11 17:53:53 -0700

    promo

--- mysite/settings.py
+++ mysite/settings.py
@@ -62,9 +62,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
